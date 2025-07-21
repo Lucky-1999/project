3413fb3b4d8ef5be910d4853804acba4f06d10dc
Lucky Chaturvedi <lucky11101999@gmail.com>
2025-07-21 18:26:23 +0530

    uploading code

---
+++ app/db.py
@@ -0,0 +1,23 @@
+from pymongo import MongoClient
+
+#client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient("mongodb+srv://luckychaturvedi11101999:UNQIqUmm2UC1zdR9@cluster0.w84rbjz.mongodb.net/")
+
+# Check and create database and collection if not exist
+db_name = "ecommerce"
+
+#collections
+product_collection_name = "products"
+order_collection_name = "orders"
+
+
+db = client[db_name]
+
+if product_collection_name not in db.list_collection_names():
+    db.create_collection(product_collection_name)
+
+if order_collection_name not in db.list_collection_names():
+    db.create_collection(order_collection_name)
+
+products_collection = db[product_collection_name]
+orders_collection = db[order_collection_name]
